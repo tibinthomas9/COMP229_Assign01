5076dc1757b1cd6f013096c182b4edb5186f01fb
Tibin Thomas <tibinmutholy@gmail.com>
2021-02-14 01:17:40 +0530

    services and projects pages added

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,14 @@ router.get('/home', function(req, res, next) {
 router.get('/aboutme', function(req, res, next) {
   res.render('aboutme', { title: 'Tibin Thomas' });
 });
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Tibin Thomas' });
+});
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Tibin Thomas' });
+});
+router.get('/contactme', function(req, res, next) {
+  res.render('contactme', { title: 'Tibin Thomas' });
+});
 
 module.exports = router;
