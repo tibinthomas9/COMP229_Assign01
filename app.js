473f97ca5922fa5b822525459656c052187f64ff
Tibin Thomas <tibinmutholy@gmail.com>
2021-02-13 17:12:56 +0530

    added partials

--- app.js
+++ app.js
@@ -37,5 +37,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 5000;
+}
+app.listen(port, () => {
+    console.log('Listening to port 3000.');
+});
 
 module.exports = app;
